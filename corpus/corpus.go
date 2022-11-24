283c9112e7da10c925d0749c927b9275cf1d4d7e
vuthitam <tamvuthi2000@gmail.com>
2022-11-25 00:31:54 +0700

    update method

--- corpus/corpus.go
+++ corpus/corpus.go
@@ -1,5 +1,36 @@
 package corpus
 
-func HashMap() {
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func HashTable() map[string]int {
+	corpus := make(map[string]int)
+
 	// Buld corpus hash table from a book
+	wordlist, err := os.Open("./resource/english_world.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer wordlist.Close()
+	scanner := bufio.NewScanner(wordlist)
+	for scanner.Scan() {
+		word := scanner.Text()
+
+		if _, ok := corpus[word]; ok {
+			//exist in corpus
+			corpus[word]++
+		} else {
+			//not exist in corpus
+			corpus[word] = 1
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return corpus
 }
--- main/main.go
+++ main/main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+
 	fmt.Printf("Enter the domain string: ")
 	var s string
 	fmt.Scanf("%s", &s)
@@ -16,16 +17,36 @@ func main() {
 		fmt.Printf(slice[i] + "\n")
 	}
 
-	CheckIsNotRanDom(slice)
+	//CheckIsNotRanDom(slice)
+
 }
+
 func CheckIsNotRanDom(s []string) {
 	for i := 0; i < len(s); i++ {
 		substring := s[i]
-		NGram(substring) // n-gram string thành các substring độ dài 3-len(string[i])
+
+		for num := 1; num <= len(substring); num++ {
+			n_words := NGram(substring, num)
+			for _, word := range n_words{
+				if CheckInCorpus(word)
+			}
+			// n-gram string thành các substring độ dài num-len(string[i])
+		}
+
 	}
-
 }
+func CheckInCorpus(corpus map[string]int ,s string) bool{
+	_, ok:= corpus[s]
+	return ok
+}
+func NGram(s string, num int) []string {
+	var array []string
+	slice := strings.Split(s, "")
 
-func NGram(s string) {
-
+	for i := 0; i < len(s)-num; i++ {
+		newslice := slice[i : i+num]
+		ngram := strings.Join(newslice, "")
+		array = append(array, ngram)
+	}
+	return array
 }
